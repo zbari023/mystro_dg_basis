c164cc531141d275226bceac5a8f3de721e4b936
zbari023 <barry450643@gmail.com>
2023-06-14 23:13:55 +0200

    Add tags

--- post/models.py
+++ post/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 class Autor(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=15000)
+    image = models.ImageField(upload_to='autor')
     def __str__(self):
         return self.name
     
@@ -13,6 +15,10 @@ class mypost(models.Model):
     title = models.CharField(max_length=100)
     publishedDate = models.DateTimeField()
     content = models.TextField(max_length=15000)
+    image = models.ImageField(upload_to='post')
+     
     autor = models.ForeignKey(Autor,related_name='mypost_Autor',on_delete=models.CASCADE)
+    
+    tags = TaggableManager()
     def __str__(self):
         return self.title
