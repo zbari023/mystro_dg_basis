ded80f9ccd46f9db976f51248518b53fedc83372
zbari023 <barry450643@gmail.com>
2023-06-15 19:16:04 +0200

    admin panal

--- post/admin.py
+++ post/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import mypost , Autor
-admin.site.register(mypost)
+class mypostAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'publishedDate','autor']
+    list_filter =  ['autor']
+    search_fields = ['autor']
+admin.site.register(mypost, mypostAdmin)
 admin.site.register(Autor)
